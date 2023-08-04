61861dbcce0fe4dd7780599148c24b273e3509da
Brian <brian18003427@gmail.com>
2023-08-03 23:56:19 -0700

    navbar in progress

--- portfolio/src/components/navbar/navbar.jsx
+++ portfolio/src/components/navbar/navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const navbar = () => {
   return (
-    <div>navbar</div>
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" style={{ flexGrow: 1 }}>
+          My Portfolio
+        </Typography>
+        <Button color="inherit" component={Link} to="/home">Home</Button>
+        <Button color="inherit" component={Link} to="/projects">Projects</Button>
+        <Button color="inherit" component={Link} to="/education">Education</Button>
+        <Button color="inherit" component={Link} to="/experience">Experience</Button>
+      </Toolbar>
+    </AppBar>
   )
 }
 
