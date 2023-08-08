f91fd4a13121b2007c69cf23e071bccf61f7a43d
Brian <brian18003427@gmail.com>
2023-08-08 00:25:14 -0700

    restructure routes to App.jsx, update gitignore, add layout to display different component

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,18 +1,25 @@
 import { Container } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
-import Navbar from "./components/navbar/navbar";
-import Footer from "./components/footer/footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import React from "react";
-import PageRoutes from "./routes/PageRoutes";
+import HomePage from "./pages/HomePage/HomePage";
+import ProjectsPage from "./pages/ProjectsPage/ProjectsPage";
+import EducationPage from "./pages/EducationPage/EducationPage";
+import ExperiencePage from "./pages/ExperiencePage/ExperiencePage";
+import LandingPage from "./pages/LandingPage/LandingPage";
+import Layout from "./layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Container maxWidth="xl">
-        <Navbar />
-        <PageRoutes />
-        <Footer />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/home" element={<Layout><HomePage /></Layout>} />
+          <Route path="/projects" element={<Layout><ProjectsPage /></Layout>} />
+          <Route path="/education" element={<Layout><EducationPage /></Layout>} />
+          <Route path="/experience" element={<Layout><ExperiencePage /></Layout>} />
+        </Routes>
       </Container>
     </BrowserRouter>
   );
