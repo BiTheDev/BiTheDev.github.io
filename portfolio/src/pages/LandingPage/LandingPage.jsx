9ea4045ca1f062145cec58f74bac58a72036289b
Brian <brian18003427@gmail.com>
2023-08-08 01:03:33 -0700

    sample design for landing page

--- portfolio/src/pages/LandingPage/LandingPage.jsx
+++ portfolio/src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Typography, Button } from '@mui/material';
+import { Container, Typography, Button, } from '@mui/material';
 import './landing.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
@@ -16,13 +16,15 @@ const LandingPage = () => {
           Showcasing my journey in software and web development.
         </Typography>
         <Button
-          data-aos="fade-in"
+          data-aos="zoom-in-up"
+          data-aos-duration="1500"
+          data-aos-easing="ease-in-sine"
           variant="contained"
           color="primary"
-          href="/portfolio"
-          className="cta-button"
+          href="/home"
+          sx={{ mx: 'auto', width: 200, p:1, m:1 }}
         >
-          View Portfolio
+          Welcome
         </Button>
       </Container>
     </div>
