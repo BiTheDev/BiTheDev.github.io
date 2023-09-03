d32c2f53abb3cf7fd40d7712b899acc5308f8914
Brian Ho <brian@MacBook-Pro.local>
2023-09-03 16:01:27 -0700

    shooting star update

--- portfolio/src/components/shootingstars/Shootingstars.jsx
+++ portfolio/src/components/shootingstars/Shootingstars.jsx
@@ -2,22 +2,33 @@ import React, { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
+function Portal() {
+  // Create a torus geometry for the portal
+  return (
+    <mesh position={[0, 0, 0]}>
+      <torusGeometry args={[10, 0.5, 16, 100]} />
+      <meshBasicMaterial color="purple" />
+    </mesh>
+  );
+}
+
 function ShootingStar() {
   const mesh = useRef();
   const line = useRef();
 
-  // Generate a random point on a circle in the X-Z plane
-  const randomCirclePoint = (radius) => {
+  // Generate a random point on the border of a torus (which acts as our portal)
+  const randomTorusPoint = (radius, tube) => {
     const theta = 2 * Math.PI * Math.random();
-    const x = radius * Math.cos(theta);
-    const y = 0;  // y-coordinate remains the same to keep it in a plane
-    const z = radius * Math.sin(theta);
+    const phi = 2 * Math.PI * Math.random();
+    const x = (radius + tube * Math.cos(phi)) * Math.cos(theta);
+    const y = (radius + tube * Math.cos(phi)) * Math.sin(theta);
+    const z = tube * Math.sin(phi);
     return { x, y, z };
   };
 
-  const initialPosition = randomCirclePoint(10);  // Starting circle with radius 10
-  const [position, setPosition] = useState(initialPosition);
-  
+  const initialPosition = randomTorusPoint(10, 0.5);
+  const [position] = useState(initialPosition);
+
   // Initial tail geometry
   const initialTailGeometry = new THREE.BufferGeometry().setFromPoints([
     new THREE.Vector3(initialPosition.x, initialPosition.y, initialPosition.z),
@@ -26,21 +37,22 @@ function ShootingStar() {
 
   useFrame(() => {
     if (mesh.current && line.current) {
-      const speed = 0.003;
+      const speed = 0.02;
       mesh.current.position.x += position.x * speed;
       mesh.current.position.y += position.y * speed;
       mesh.current.position.z += position.z * speed;
 
-      // Update tail geometry
+      // Update tail geometry to make it longer
+      const tailLength = 3;
       const newTailGeometry = new THREE.BufferGeometry().setFromPoints([
         new THREE.Vector3(mesh.current.position.x, mesh.current.position.y, mesh.current.position.z),
         new THREE.Vector3(
-          mesh.current.position.x - position.x * 0.1,
-          mesh.current.position.y - position.y * 0.1,
-          mesh.current.position.z - position.z * 0.1
+          mesh.current.position.x - position.x * tailLength * speed,
+          mesh.current.position.y - position.y * tailLength * speed,
+          mesh.current.position.z - position.z * tailLength * speed
         ),
       ]);
-      line.current.geometry.dispose();  // dispose of old geometry
+      line.current.geometry.dispose();
       line.current.geometry = newTailGeometry;
 
       const distance = Math.sqrt(
@@ -49,7 +61,7 @@ function ShootingStar() {
         mesh.current.position.z ** 2
       );
 
-      if (distance > 40) {
+      if (distance > 100) {
         mesh.current.position.set(0, 0, 0); // Reset to origin
       }
     }
@@ -59,7 +71,7 @@ function ShootingStar() {
     <>
       <mesh ref={mesh} position={[initialPosition.x, initialPosition.y, initialPosition.z]}>
         <sphereGeometry args={[0.1, 16, 16]} />
-        <meshBasicMaterial color="white" />
+        <meshBasicMaterial color="skyblue" />
       </mesh>
       <line ref={line} geometry={initialTailGeometry}>
         <lineBasicMaterial attach="material" color="white" />
@@ -68,12 +80,13 @@ function ShootingStar() {
   );
 }
 
-const Shootingstars = () => {
+const ShootingStars = () => {
   const numStars = 500;
   const stars = new Array(numStars).fill(null);
 
   return (
     <Canvas style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <Portal />
       {stars.map((_, index) => (
         <ShootingStar key={index} />
       ))}
@@ -81,4 +94,4 @@ const Shootingstars = () => {
   );
 };
 
-export default Shootingstars;
+export default ShootingStars;
