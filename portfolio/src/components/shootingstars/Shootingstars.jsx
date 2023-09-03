a8374e5a4e979b53f0d6c006ae75630e1a82a2fb
Brian Ho <brian@MacBook-Pro.local>
2023-09-03 16:33:46 -0700

    portal added, shooting star updated

--- portfolio/src/components/shootingstars/Shootingstars.jsx
+++ portfolio/src/components/shootingstars/Shootingstars.jsx
@@ -1,79 +1,76 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 
+function SetCamera() {
+  useFrame(({ camera }) => {
+    camera.position.z = 50;
+  });
+  return null;
+}
+
 function Portal() {
-  // Create a torus geometry for the portal
   return (
     <mesh position={[0, 0, 0]}>
       <torusGeometry args={[10, 0.5, 16, 100]} />
-      <meshBasicMaterial color="purple" />
+      <meshStandardMaterial color="purple" emissive="purple" emissiveIntensity={0.5} />
     </mesh>
   );
 }
 
-function ShootingStar() {
+function ShootingStar({ theta, phi }) {
   const mesh = useRef();
   const line = useRef();
+  const [position, setPosition] = useState(new THREE.Vector3());
+  const [currentTailLength, setCurrentTailLength] = useState(1);
 
-  // Generate a random point on the border of a torus (which acts as our portal)
-  const randomTorusPoint = (radius, tube) => {
-    const theta = 2 * Math.PI * Math.random();
-    const phi = 2 * Math.PI * Math.random();
+  useEffect(() => {
+    const radius = 10;
+    const tube = 0.5;
     const x = (radius + tube * Math.cos(phi)) * Math.cos(theta);
     const y = (radius + tube * Math.cos(phi)) * Math.sin(theta);
     const z = tube * Math.sin(phi);
-    return { x, y, z };
-  };
-
-  const initialPosition = randomTorusPoint(10, 0.5);
-  const [position] = useState(initialPosition);
-
-  // Initial tail geometry
-  const initialTailGeometry = new THREE.BufferGeometry().setFromPoints([
-    new THREE.Vector3(initialPosition.x, initialPosition.y, initialPosition.z),
-    new THREE.Vector3(initialPosition.x, initialPosition.y, initialPosition.z),
-  ]);
+    setPosition(new THREE.Vector3(x, y, z));
+  }, [theta, phi]);
 
   useFrame(() => {
     if (mesh.current && line.current) {
-      const speed = 0.02;
-      mesh.current.position.x += position.x * speed;
-      mesh.current.position.y += position.y * speed;
-      mesh.current.position.z += position.z * speed;
+      const speed = 0.05;
+      mesh.current.position.addScaledVector(position, speed);
 
-      // Update tail geometry to make it longer
-      const tailLength = 3;
+      const portalRadius = 5;
+      const maxTailLength = 150;
+      const distance = mesh.current.position.length();
+
+      if (distance > portalRadius) {
+        setCurrentTailLength(Math.min(currentTailLength + 0.9, maxTailLength));
+      }
+
+      const tailPosition = position.clone().multiplyScalar(-currentTailLength * speed);
+      tailPosition.add(mesh.current.position);
+
       const newTailGeometry = new THREE.BufferGeometry().setFromPoints([
-        new THREE.Vector3(mesh.current.position.x, mesh.current.position.y, mesh.current.position.z),
-        new THREE.Vector3(
-          mesh.current.position.x - position.x * tailLength * speed,
-          mesh.current.position.y - position.y * tailLength * speed,
-          mesh.current.position.z - position.z * tailLength * speed
-        ),
+        mesh.current.position.clone(),
+        tailPosition,
       ]);
       line.current.geometry.dispose();
       line.current.geometry = newTailGeometry;
 
-      const distance = Math.sqrt(
-        mesh.current.position.x ** 2 +
-        mesh.current.position.y ** 2 +
-        mesh.current.position.z ** 2
-      );
-
       if (distance > 100) {
-        mesh.current.position.set(0, 0, 0); // Reset to origin
+        mesh.current.position.copy(position);
+        setCurrentTailLength(1);
       }
     }
   });
 
   return (
     <>
-      <mesh ref={mesh} position={[initialPosition.x, initialPosition.y, initialPosition.z]}>
+      <mesh ref={mesh} position={position.toArray()}>
         <sphereGeometry args={[0.1, 16, 16]} />
-        <meshBasicMaterial color="skyblue" />
+        <meshStandardMaterial color="skyblue" />
       </mesh>
-      <line ref={line} geometry={initialTailGeometry}>
+      <line ref={line}>
+        <bufferGeometry attach="geometry" />
         <lineBasicMaterial attach="material" color="white" />
       </line>
     </>
@@ -86,10 +83,14 @@ const ShootingStars = () => {
 
   return (
     <Canvas style={{ position: 'fixed', top: 0, left: 0, width: '100%', height: '100%' }}>
+      <ambientLight intensity={0.5} />
+      <SetCamera />
       <Portal />
-      {stars.map((_, index) => (
-        <ShootingStar key={index} />
-      ))}
+      {stars.map((_, index) => {
+        const theta = (index / numStars) * Math.PI * 2;
+        const phi = Math.acos((index / numStars) * 2 - 1);
+        return <ShootingStar key={index} theta={theta} phi={phi} />;
+      })}
     </Canvas>
   );
 };
