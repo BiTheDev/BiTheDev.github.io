66a894fec13b9b8117ef9c5bc2bb298e19182e2f
Brian Ho <brian@MacBook-Pro.local>
2023-09-05 14:50:59 -0700

    update content

--- portfolio/src/components/navbar/navbar.jsx
+++ portfolio/src/components/navbar/navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <AppBar position="static" style={{background: "linear-gradient(to bottom, #151515 0%, #263238 100%)"}}>
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}>
-          My Portfolio
+          Brian Ho's Portfolio
         </Typography>
         <Button color="inherit" component={Link} to="/home">Home</Button>
         <Button color="inherit" component={Link} to="/projects">Projects</Button>
