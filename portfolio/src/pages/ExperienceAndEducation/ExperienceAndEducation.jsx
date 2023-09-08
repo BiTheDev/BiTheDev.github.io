12da7a03c6a54adb011c03f558e0b57e07d98df7
Brian Ho <brian@Brians-MacBook-Pro.local>
2023-09-07 21:59:24 -0700

    update education conent

--- portfolio/src/pages/ExperienceAndEducation/ExperienceAndEducation.jsx
+++ portfolio/src/pages/ExperienceAndEducation/ExperienceAndEducation.jsx
@@ -14,21 +14,51 @@ const ExperienceAndEducation = () => {
       degree: "Master of Computer Science",
       year: "June 2024",
       institution: "Northeastern University, Khoury College of Computer Sciences | Seattle, WA",
+      description:[
+        "Relevant courses completed: Algorithm, Programming Design Paradigm, Database Management, Scalable Distributed System, Foundation of Software development, Web development (React)",
+        "Course in progress: Machine Learning"
+      ]
     },
     {
       degree: "Bachelor of Geography - GIS, Mapping and Society, Minor Informatics",
       year: "August 2020",
       institution: "University of Washington | Seattle, WA",
+      description:[
+        "Relevant courses completed: GIS Mapping, GIS Database Programming, Tech Foundations with R, Coastal GIS, Database and Data Modeling, Design Methods, data programming"
+      ]
     },
+    {
+      degree: "Web Development Certificate ",
+      year: "September 2018",
+      institution: "Coding Dojo Bootcamp | Bellevue, WA",
+      description:[
+        "Spent a total of 1000+ hours on countless assignments and projects",
+        "Stack Learned: ",
+        "C#/ASP.NET ( Black Belt - 100%)",
+        "MongoDB/Express.js/Angular/Node.js (Black Belt - 100%)",
+        "Python/Django ( Red Belt - 90%)"
+      ]
+    },
+    {
+      degree: "Associate of Arts and Science",
+      year: "December 2017",
+      institution: "Bellevue College | Bellevue, WA",
+      description:[
+        "Relevant courses completed: SQL Fundamentals, Networking Basics, Web Development, Systems Analysis & Design, C# programming, JAVA programming"
+      ]
+    }
   ];
 
   const experienceData = [
     {
       title: "Associate Software Engineer",
-      year: "January 2021 – April 2023",
+      year: "January 2021 - April 2023",
       company: "Infosys | Bellevue, WA (Remote)",
       description: [
-        "Revamped tracking mechanisms, improving efficiency by 70% across 52 markets for Bing Wallpaper online content.",
+        "Revamped tracking mechanisms, improving efficiency by 70% across 52 markets for Bing Wallpaper online content. ",
+        "Analyzed and documented intricate user experience bug reports across multiple browsers and operating systems. ",
+        "Developed user-centric features for Bing's new tab extension using HTML, CSS, jQuery, AJAX, and Chrome Extension API. ",
+        "Fostered a collaborative remote working environment and consistently improved the quality of Bing Search Chrome Extension development. ",
         // Add more descriptions
       ],
     },
@@ -55,6 +85,15 @@ const ExperienceAndEducation = () => {
                 <Typography variant="body1" gutterBottom>
                   {edu.institution}
                 </Typography>
+                <ul>
+                  {edu.description.map((desc, i) => (
+                    <li key={i}>
+                      <Typography variant="body2">
+                        {desc}
+                      </Typography>
+                    </li>
+                  ))}
+                </ul>
                 <Divider style={{ marginBottom: '16px', marginTop: '16px' }} />
               </div>
             ))}
